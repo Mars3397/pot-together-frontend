6f7b67c55fdcec2a523cfa3a8fac6084a9e27c8b
Mars3397 <mars3397@gmail.com>
2023-12-02 01:23:56 +0800

    add routes for different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from 'Login'
 import Overview from './Overview'
+import Room from './Room'
+import CreatePot from 'CreatePot'
+import SearchPot from 'SearchPot'
 
 const App = () => {
   return (
-    <Overview />
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Overview />} />
+        <Route path='/room/:roomId' element={<Room />} />
+        <Route path='/CreatePot' element={<CreatePot />} />
+        <Route path='/SearchPot' element={<SearchPot />} />
+      </Routes>
+    </Router>
   )
 }
 
