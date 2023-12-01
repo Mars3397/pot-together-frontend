1c64d87df52db8ebac397f49b336e72040debd24
Mars3397 <mars3397@gmail.com>
2023-12-02 01:24:13 +0800

    Create the basic overview page

--- src/Overview/index.tsx
+++ src/Overview/index.tsx
@@ -1,9 +1,13 @@
+import Header from 'CommonComponents/header'
 import './Overview.css'
 
 const Overview = () => {
     return (
         <div id="overview">
-            
+            <Header title='Overview' />
+            <div className='content'>
+                <p>content</p>
+            </div>
         </div>
     )
 }
