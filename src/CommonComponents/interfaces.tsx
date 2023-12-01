326d5d13cc5990362db7392d49870209e815aabe
Mars3397 <mars3397@gmail.com>
2023-12-02 01:22:13 +0800

    Create common interfaces board

---
+++ src/CommonComponents/interfaces.tsx
@@ -0,0 +1,11 @@
+/**
+ * Interfaces for the components
+ * Eg. API response, function parameters ...
+ */
+
+export interface MenuItem {
+    id: string
+    name: string
+    online: number
+    total: number
+}
