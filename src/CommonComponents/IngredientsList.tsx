76dc054841fe15731d0cf8fc55c69c077c153bce
Mars3397 <mars3397@gmail.com>
2023-12-02 17:14:31 +0800

    Create common ingredient list

---
+++ src/CommonComponents/IngredientsList.tsx
@@ -0,0 +1,31 @@
+import { ReactComponent as Mushrooms } from '../assets/Mushrooms.svg'
+import './Common.css'
+
+interface IngredientsListProps {
+    title: string
+}
+
+const IngredientsList = (props: IngredientsListProps) => {
+    const { title } = props
+
+    return (
+        <div className='ingredients-list'>
+            <div className='list-title'>
+                <span className='list-title-text'>{title}</span>
+            </div>
+            <div className='list-content'>
+                <div className='list-item'>
+                    <Mushrooms />
+                </div>
+                <div className='list-item'>
+                    <Mushrooms />
+                </div>
+                <div className='list-item'>
+                    <Mushrooms />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default IngredientsList
