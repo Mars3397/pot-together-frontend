3307f382f69a571eaa2d99e5e169fe4055883920
ting0602 <ting0602.cs09@nycu.edu.tw>
2023-12-04 23:09:14 +0800

    Create GroupAnalysis && fix Members

---
+++ src/components/Room/GroupAnalysis.tsx
@@ -0,0 +1,59 @@
+import { BarChart } from '@mui/x-charts/BarChart'
+import './Room.css'
+
+interface GroupAnalysisProps {
+    duration: number,
+    targetYear: number,
+    targetMonth: number,
+    targetDay: number
+}
+
+const colorList = ["#246270", "#63623A", "#8A4F3E", "#36423C"]
+
+function calculatePreviousSevenDays(targetYear: number, targetMonth: number, targetDay: number) {
+    // Create a Date object for the target date
+    const targetDate = new Date(targetYear, targetMonth - 1, targetDay);
+
+    // Calculate the previous seven days
+    const previousDays = [];
+    for (let i = 6; i >= 0; i--) {
+        const date = new Date(targetDate);
+        date.setDate(targetDate.getDate() - i);
+        // Use template literals to format MM/DD
+        const formattedDate = `${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
+        previousDays.push(formattedDate);
+    }
+    return previousDays;
+}
+
+
+function GroupAnalysis(props: GroupAnalysisProps) {
+    const { duration, targetYear, targetMonth, targetDay } = props
+    const previousDays = calculatePreviousSevenDays(targetYear, targetMonth, targetDay)
+    return (
+        <div id='group-analysis'>
+            <div className='group-analysis-content'>
+                <div className='group-analysis-title'>
+                    <span className='group-analysis-title-text'>{duration}&nbsp;hr</span>
+                    <span className='group-analysis-title-date'>{previousDays[0]} ~ {previousDays[6]}</span>
+                </div>
+                <BarChart 
+                    leftAxis={null}
+                    bottomAxis={null}
+                    xAxis={[{ scaleType: 'band', data: previousDays }]}
+                    series={[{ data: [4, 3, 5, 7, 12, 14, 1], color: colorList[0]}, { data: [1, 6, 3, 3, 18, 7, 12], color: colorList[1] }, { data: [12, 5, 6, 1, 5, 7, 12], color: colorList[2] }, { data: [12, 0, 3, 8, 5, 2, 10], color: colorList[3] }]}
+                    width={320}
+                    height={100}
+                    margin={{
+                        left: 10,
+                        right: 20,
+                        top: 20,
+                        bottom: 20,
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default GroupAnalysis
--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -3,6 +3,7 @@ import Header from 'components/CommonComponents/Header'
 import IngredientsList from 'components/CommonComponents/IngredientsList'
 import TimeProgress from '../CommonComponents/TimeProgress'
 import Members from './Members'
+import GroupAnalysis from './GroupAnalysis'
 import './Room.css'
 
 const Room = () => {
@@ -14,15 +15,14 @@ const Room = () => {
             <Header title={String(roomId)} /> 
             <div className='content'>
                 <Members />
+                <GroupAnalysis duration={100} targetYear={2023} targetMonth={1} targetDay={2}/>
                 <TimeProgress 
                     title="Total Cooking Duration " 
-                    duration={170}
+                    duration={92}
                     target={200}
                 />
                 <IngredientsList title="Now Cooking..." />
                 <IngredientsList title="Done!" />
-
-                {/* <p>content</p> */}
             </div>
         </div>
     )
