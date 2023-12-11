98110b13c848ef608f6e42a67bf6b52b784d89c3
Mars3397 <mars3397@gmail.com>
2023-12-12 03:11:24 +0800

    add dialog to display pot info

--- src/components/SearchPot/index.tsx
+++ src/components/SearchPot/index.tsx
@@ -1,49 +1,78 @@
 import { useState } from "react"
 import HeaderWithBack from "components/CommonComponents/HeaderWithBack"
+import Dialog from "components/CommonComponents/Dialog"
 import { InputAdornment, TextField } from "@mui/material"
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded'
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded'
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded'
 import "./SearchPot.css"
+
+interface PotInfo {
+    id: number
+    name: string
+    category: string[]
+    currentMember: number
+    maxMember: number
+}
 
 const SearchPot = () => {
     const [search, setSearch] = useState("")
     const [filterCategory, setFilterCategory] = useState<string[]>([])
+    const [openDialog, setOpenDialog] = useState(false)
+    const [selectedPot, setSelectedPot] = useState<PotInfo | null>(null)
 
-    const potList = [
+    const categories = ["College", "English", "Coding", "Reading"]
+
+    const potList: PotInfo[] = [
         {
             id: 0,
             name: "Welcome!",
-            category: "Coding",
+            category: ["College", "Coding"],
             currentMember: 5,
             maxMember: 10,
         },
         {
             id: 1,
             name: "Public Pot",
-            category: "English",
+            category: ["English"],
             currentMember: 8,
             maxMember: 15,
         },
         {
             id: 2,
             name: "3hr/day",
-            category: "College",
+            category: ["College"],
             currentMember: 11,
             maxMember: 15,
         },
         {
             id: 3,
             name: "Reading...",
-            category: "Reading",
+            category: ["Reading", "English"],
             currentMember: 3,
             maxMember: 10,
         }
     ]
 
     const filteredPotList = () => {
-        return potList.filter((pot) => {
-            return pot.name.toLowerCase().includes(search.toLowerCase()) && (filterCategory.length === 0 || filterCategory.includes(pot.category))
-        })
+        if (search === "") {
+            return potList.filter((pot) => {
+                if (filterCategory.length === 0) {
+                    return true
+                } else {
+                    return filterCategory.some((category) => pot.category.includes(category))
+                }
+            })
+        } else {
+            return potList.filter((pot) => {
+                if (filterCategory.length === 0) {
+                    return pot.name.toLowerCase().includes(search.toLowerCase())
+                } else {
+                    return filterCategory.some((category) => pot.category.includes(category))
+                        && pot.name.toLowerCase().includes(search.toLowerCase())
+                }
+            })
+        }
     }
 
     const handleFilterBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -55,6 +84,11 @@ const SearchPot = () => {
             btn.classList.add("filter-btn-active")
             setFilterCategory([...filterCategory, btn.innerText])
         }
+    }
+
+    const handlePotClick = (pot: PotInfo) => {
+        setSelectedPot(pot)
+        setOpenDialog(true)
     }
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -73,31 +107,12 @@ const SearchPot = () => {
                 className="input-field"
                 margin="normal"
                 InputProps={{
-                    startAdornment: (
-                        <InputAdornment
-                            position="start"
-                            className="input-field-icon"
-                        >
-                            <SearchRoundedIcon sx={{
-                                color: '#BABEF4',
-                                stroke: "#BABEF4",
-                                strokeWidth: 1
-                            }} />
-                        </InputAdornment>
-                    ),
+                    startAdornment: <Adornment position="start" />,
                     endAdornment: (
-                        <InputAdornment
+                        <Adornment
                             position="end"
-                            className="input-field-icon"
                             onClick={() => setSearch("")}
-                        >
-                            <CloseRoundedIcon
-                                sx={{
-                                    color: '#BABEF4',
-                                    stroke: "#BABEF4",
-                                    strokeWidth: 1
-                                }} />
-                        </InputAdornment>
+                        />
                     ),
                 }}
                 inputProps={{
@@ -123,41 +138,86 @@ const SearchPot = () => {
                 }}
             />
             <div className="filter-btns">
-                <button
-                    className="filter-btn"
-                    onClick={handleFilterBtnClick}
-                >
-                    College
-                </button>
-                <button
-                    className="filter-btn"
-                    onClick={handleFilterBtnClick}
-                >
-                    English
-                </button>
-                <button
-                    className="filter-btn"
-                    onClick={handleFilterBtnClick}
-                >
-                    Coding
-                </button>
-                <button
-                    className="filter-btn"
-                    onClick={handleFilterBtnClick}
-                >
-                    Reading
-                </button>
+                {categories.map((category) => (
+                    <button
+                        key={category}
+                        className="filter-btn"
+                        onClick={handleFilterBtnClick}
+                    >
+                        {category}
+                    </button>
+                ))}
             </div>
             <div className="pot-list">
                 {filteredPotList().map((pot) => (
-                    <div className="pot" key={pot.id}>
+                    <div
+                        key={pot.id}
+                        className="pot"
+                        onClick={() => handlePotClick(pot)}
+                    >
                         <div className="pot-name">{pot.name}</div>
                         <div className="pot-member">{pot.currentMember}/{pot.maxMember}</div>
                     </div>
                 ))}
             </div>
+            <Dialog
+                open={openDialog}
+                handleClose={() => setOpenDialog(false)}
+            >
+                <div className="dialog-container">
+                    <div className="dialog-header">Pot Info</div>
+                    <div className="name-container">
+                        <FavoriteBorderRoundedIcon className="name-icon" />
+                        <span className="pot-name">{selectedPot?.name}</span>
+                    </div>
+                    <div className="category-container">
+                        {selectedPot?.category.map((category) => (
+                            <div key={category} className="category">
+                                {category}
+                            </div>
+                        ))}
+                    </div>
+                    <span className="members">
+                        Members: {selectedPot?.currentMember}/{selectedPot?.maxMember}
+                    </span>
+                    <button className="join-btn">Join Pot</button>
+                </div>
+            </Dialog>
         </div>
     )
 }
 
 export default SearchPot
+
+interface AdornmentProps {
+    position: 'start' | 'end',
+    onClick?: () => void
+}
+
+const Adornment = (props: AdornmentProps) => {
+    const { position, onClick } = props
+
+    return (
+        <InputAdornment
+            position={position}
+            className="input-field-icon"
+            onClick={onClick}
+        >
+            {position === 'start' ? (
+                <SearchRoundedIcon
+                    sx={{
+                        color: '#BABEF4',
+                        stroke: "#BABEF4",
+                        strokeWidth: 1
+                    }} />
+            ) : (
+                <CloseRoundedIcon
+                    sx={{
+                        color: '#BABEF4',
+                        stroke: "#BABEF4",
+                        strokeWidth: 1
+                    }} />
+            )}
+        </InputAdornment>
+    )
+}
