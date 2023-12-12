ff58aa7082287201dcf2a36b5279bcf1878141cf
aidencj <aidenncj@icloud.com>
2023-12-12 14:46:00 +0800

    redirect unauthenticated user to /login

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Welcome from 'components/Welcome'
-import SignUp from 'components/SignUp'
-import Login from 'components/Login'
-import Overview from 'components/Overview'
-import Room from 'components/Room'
-import CreatePot from 'components/CreatePot'
-import SearchPot from 'components/SearchPot'
-import Choose from 'components/Choose'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import routes, { RouteConfig } from 'authentication/routes';
+
+// This is for testing only.
+const isAuthenticated = false;
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/welcome' element={<Welcome />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Overview />} />
-        <Route path='/room/:roomId' element={<Room />} />
-        <Route path='/CreatePot' element={<CreatePot />} />
-        <Route path='/SearchPot' element={<SearchPot />} />
-        <Route path='/room/:roomId/Choose' element={<Choose />} />
+        {routes.map((route: RouteConfig, index: number) => (
+          !isAuthenticated && route.isPrivate ?
+            <Route key={index} path="*" element={<Navigate to="/login" />} /> :
+            <Route key={index} path={route.path} element={route.element} />
+        ))}
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   )
---
+++ src/authentication/routes.tsx
@@ -0,0 +1,60 @@
+import Welcome from 'components/Welcome'
+import SignUp from 'components/SignUp'
+import Login from 'components/Login'
+import Overview from 'components/Overview'
+import Room from 'components/Room'
+import CreatePot from 'components/CreatePot'
+import SearchPot from 'components/SearchPot'
+import Choose from 'components/Choose'
+
+export type RouteConfig = {
+  path: string;
+  element: JSX.Element;
+  isPrivate?: boolean; // Optional boolean property
+};
+
+const routes: RouteConfig[] = [
+  // Define your routes here
+  {
+    path: '/welcome',
+    element: <Welcome />,
+    isPrivate: false,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+    isPrivate: false,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    isPrivate: false,
+  },
+  {
+    path: '/',
+    element: <Overview />,
+    isPrivate: true,
+  },
+  {
+    path: '/room/:roomId',
+    element: <Room />,
+    isPrivate: true,
+  },
+  {
+    path: '/CreatePot',
+    element: <CreatePot />,
+    isPrivate: true,
+  },
+  {
+    path: '/SearchPot',
+    element: <SearchPot />,
+    isPrivate: true,
+  },
+  {
+    path: '/room/:roomId/Choose',
+    element: <Choose />,
+    isPrivate: true,
+  },
+];
+
+export default routes;
