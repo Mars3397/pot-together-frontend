9edb3d02986b5023805397f351f5862710747b0f
Mars3397 <mars3397@gmail.com>
2023-12-12 20:55:51 +0800

    handle orientation permission

--- src/components/Cooking/index.tsx
+++ src/components/Cooking/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Pot from 'assets/pot.svg';
 import Mushrooms from 'assets/Mushrooms.svg';
 import './Cooking.css';
@@ -10,23 +10,14 @@ const contentList = [
     'The timer has been paused.',
 ];
 
-var count = 0
-
 const Cooking = () => {
     const [targetTime, setTargetTime] = useState(initTime * 60);
     const [contentIndex, setContentIndex] = useState(0);
     const [isFaceUp] = useState(false);
-    // const [isFaceUp, setIsFaceUp] = useState(false);
-    
-    // const alphaRef = useRef(90);
-    // const betaRef = useRef(90);
-    // const gammaRef = useRef(90);
 
     const [alpha, setAlpha] = useState(90);
     const [beta, setBeta] = useState(90)
     const [gamma, setGamma] = useState(90)
-
-
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -52,58 +43,26 @@ const Cooking = () => {
         return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     };
 
-    useEffect(() => {
-        const handleOrientationChange = (event: DeviceOrientationEvent) => {
-            count += 1
-            const alphaBuf = event.alpha || 0;
-            const betaBuf = event.beta || 0;
-            const gammaBuf = event.gamma || 0;
-            // const alpha = event.alpha !== null && event.alpha !== undefined ? event.alpha : 0;
-            // const beta = event.beta !== null && event.beta !== undefined ? event.beta : 0;
-            // const gamma = event.gamma !== null && event.gamma !== undefined ? event.gamma : 0;
-            
-            setAlpha(alphaBuf);
-            setBeta(betaBuf);
-            setGamma(gammaBuf);
-        };
+    const startGyro = () => {
+        const requestPermission = (DeviceOrientationEvent as any).requestPermission;
+        if (typeof requestPermission === 'function') {
+            requestPermission()
 
-        if (window.DeviceOrientationEvent !== undefined) {
-            window.addEventListener('deviceorientation', handleOrientationChange);
-
-            return () => {
-                setAlpha(-10);
-                setBeta(-10);
-                setGamma(-10);
-
-                window.removeEventListener('deviceorientation', handleOrientationChange);
+            const handleOrientationChange = (event: DeviceOrientationEvent) => {
+                setAlpha(Math.round(event.alpha || 0));
+                setBeta(Math.round(event.beta || 0));
+                setGamma(Math.round(event.gamma || 0));
             };
-        } else {
-            console.log('Your browser does not support device orientation.');
+
+            window.addEventListener('deviceorientation', handleOrientationChange);
         }
-    }, [alpha, beta, gamma]);
-    // useEffect(() => {
-    //     const testFunc = (event: MouseEvent) => {
-    //         count += 1
-            
-    //     };
-
-    //     window.addEventListener('click', testFunc);
-
-    //     return () => {
-    //         alphaRef.current = -10;
-    //         betaRef.current = -10;
-    //         gammaRef.current = -10;
-
-    //         window.removeEventListener('click', testFunc);
-    //     };
-
-    // });
+    };
 
     return (
         <div id="cooking">
-            {/* <span className="content" style={{ fontSize: '3.5rem', letterSpacing: '.2rem' }}>
+            <span className="content" style={{ fontSize: '3.5rem', letterSpacing: '.2rem' }}>
                 {targetTime >= 0 ? formatTime(targetTime) : '+' + formatTime(-targetTime)}
-            </span> */}
+            </span>
             <span className="content">{contentList[contentIndex]}</span>
             <div className="cooking-img">
                 <img className="cooking-ingredient" src={Mushrooms} alt="" />
@@ -114,8 +73,10 @@ const Cooking = () => {
                 Turn the phone over<br />
                 to start cooking
             </span>
-            <span>value:{count}</span>
-            <span>value:{alpha}/{beta}/{gamma}</span>
+            <button onClick={startGyro}>Start Gyro</button>
+            <span>alpha:{alpha}</span><br />
+            <span>beta:{beta}</span><br />
+            <span>gamma:{gamma}</span><br />
             <div style={{ marginBlock: '1rem' }}>
                 <a href="/" className="done-btn">
                     Done
