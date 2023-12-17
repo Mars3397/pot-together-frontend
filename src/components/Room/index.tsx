edd964b09ec4a23511f95bc8b757f89c3ee4df4c
Mars3397 <mars3397@gmail.com>
2023-12-17 22:43:29 +0800

    use API data in header

--- src/components/Room/index.tsx
+++ src/components/Room/index.tsx
@@ -5,30 +5,45 @@ import TimeProgress from '../CommonComponents/TimeProgress/TimeProgress'
 import Members from './Members'
 import GroupAnalysis from './GroupAnalysis'
 import BottomMenu from './BottomMenu'
+import { useAllUserRooms } from 'hooks/useRoom'
+import CircularProgress from '@mui/material/CircularProgress';
 import './Room.css'
 
 const Room = () => {
     const { roomId } = useParams()
+    const {
+        data: roomData,
+    } = useAllUserRooms()
 
     return (
         <div id="room">
-            {/* Room name should be retrived from API data (handle later) */}
-            <Header title={String(roomId)} /> 
-            <div className='content'>
-                <Members />
-                <GroupAnalysis duration={100} targetYear={2023} targetMonth={1} targetDay={2}/>
-                <TimeProgress 
-                    title="Total Cooking Duration " 
-                    duration={92}
-                    target={200}
-                />
-                <IngredientsList title="Now Cooking..." />
-                <IngredientsList title="Done!" />
-                <div id='dish-up-btn'>
-                    DISH UP
-                </div>
-            </div>
-            <BottomMenu />
+            {roomData === undefined ? (
+                <CircularProgress sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '45%',
+                    transform: 'translate(-50%, -50%)'
+                }} />
+            ) : (
+                <>
+                    <Header title={String(roomId)} roomData={roomData.data} />
+                    <div className='content'>
+                        <Members />
+                        <GroupAnalysis duration={100} targetYear={2023} targetMonth={1} targetDay={2} />
+                        <TimeProgress
+                            title="Total Cooking Duration "
+                            duration={92}
+                            target={200}
+                        />
+                        <IngredientsList title="Now Cooking..." />
+                        <IngredientsList title="Done!" />
+                        <div id='dish-up-btn'>
+                            DISH UP
+                        </div>
+                    </div>
+                    <BottomMenu />
+                </>
+            )}
         </div>
     )
 }
