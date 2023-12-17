b27d0b4683f2c432cdc007aeb8e49daba6b1a946
Mars3397 <mars3397@gmail.com>
2023-12-17 22:40:11 +0800

    add get overview api

--- src/api/User.ts
+++ src/api/User.ts
@@ -1,6 +1,13 @@
-import API from "api";
 import axios from "axios";
+import API from "api";
 
+/**
+ * structure for Signup request data
+ * @field avatar: avatar number
+ * @field name: user name
+ * @field email: user email
+ * @field password: user password
+ */
 interface SignupData {
     avatar: number;
     name: string;
@@ -17,6 +24,11 @@ export const Signup = async (data: SignupData) => {
     return response.data;
 }
 
+/**
+ * structure for Login request data
+ * @field email: user email
+ * @field password: user password
+ */
 interface LoginData {
     email: string;
     password: string;
@@ -27,6 +39,14 @@ export const Login = async (data: LoginData) => {
         method: "POST",
         url: API.login,
         data,
+    });
+    return response.data;
+}
+
+export const GetOverview = async () => {
+    const response = await axios({
+        method: "GET",
+        url: API.overview,
     });
     return response.data;
 }
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,7 +1,13 @@
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
-import { Signup, Login } from "api/User";
+import { Signup, Login, GetOverview } from "api/User";
 import { useAuth } from "provider/AuthProvider";
+import {
+    APIResponse,
+    LevelInfo,
+    RecordWithImage,
+    DailyLength,
+} from "api";
 
 export const useSignup = () => {
     const navigate = useNavigate();
@@ -24,8 +30,36 @@ export const useLogin = () => {
     return useMutation(Login, {
         onSuccess: (data) => {
             console.log(data);
-            setToken(data.data.token); // set token to local storage
+            setToken(data.data); // set token to local storage
             navigate("/");
+        },
+        onError: (error) => {
+            console.log(error);
+        },
+    });
+}
+
+/**
+ * structure for GetOverview response
+ * @field userID: user id
+ * @field level: level info
+ * @field today: today's cooking records
+ * @field week: this week's daily accumulated cooking time
+ * @field month: this month's daily accumulated cooking time
+ */
+interface GetOverviewResponse {
+    userID: number;
+    level: LevelInfo;
+    today: RecordWithImage[];
+    week: DailyLength[];
+    month: DailyLength[];
+}
+
+export const useGetOverview = () => {
+    return useQuery<APIResponse<GetOverviewResponse>>(
+        "overview", GetOverview, {
+        onSuccess: (data) => {
+            console.log(data);
         },
         onError: (error) => {
             console.log(error);
