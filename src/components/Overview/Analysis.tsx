9a9a20abd95f109b5ea332cb435b8b15f18f242a
Mars3397 <mars3397@gmail.com>
2024-01-01 11:36:46 +0800

    change to real time

--- src/components/Overview/Analysis.tsx
+++ src/components/Overview/Analysis.tsx
@@ -2,6 +2,20 @@ import { BarChart } from '@mui/x-charts/BarChart'
 import './Overview.css'
 
 const Analysis = () => {
+    const todayDateString = new Date().toLocaleDateString()
+
+    const lastWeekDateString = [
+        new Date(new Date().setDate(new Date().getDate() - 6)).toLocaleDateString(),
+        new Date(new Date().setDate(new Date().getDate() - 5)).toLocaleDateString(),
+        new Date(new Date().setDate(new Date().getDate() - 4)).toLocaleDateString(),
+        new Date(new Date().setDate(new Date().getDate() - 3)).toLocaleDateString(),
+        new Date(new Date().setDate(new Date().getDate() - 2)).toLocaleDateString(),
+        new Date(new Date().setDate(new Date().getDate() - 1)).toLocaleDateString(),
+        new Date().toLocaleDateString(),
+    ]
+
+    console.log(todayDateString.substring(5, todayDateString.length))
+
     return (
         <div id='analysis'>
             <div className='analysis-title'>
@@ -9,7 +23,9 @@ const Analysis = () => {
             </div>
             <div className='analysis-content'>
                 <div className='date'>
-                    <span className='date-text'>11/19</span>
+                    <span className='date-text'>
+                        {todayDateString.substring(5, todayDateString.length)}
+                    </span>
                 </div>
                 <div className='time'>
                     <span className='time-text'>3:50:00</span>
@@ -20,7 +36,7 @@ const Analysis = () => {
                     // tooltip={{ trigger: 'none' }}
                     xAxis={[{
                         scaleType: 'band',
-                        data: ['12/1', '12/2', '12/3', '12/4', '12/5', '12/6', '12/7'],
+                        data: lastWeekDateString,
                     }]}
                     series={[
                         { data: [2, 3, 4, 5, 6, 1, 0], stack: 'A' },
